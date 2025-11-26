bcc4aa047b7f7e028a7b191f0d0cd52a174fe7f5
Ellian <dbsrn0125@naver.com>
2025-11-26 22:35:44 +0900

    Update partners: Add RISE as #2, shift Misumi/Meviy to #3/#4

--- zenith-web/src/data/partners.ts
+++ zenith-web/src/data/partners.ts
@@ -6,7 +6,8 @@ export interface Partner {
 
 export const partnersData: Partner[] = [
     { name: 'SeoulTech', logo: '/assets/partners/1.png', link: 'https://www.seoultech.ac.kr/' },
-    { name: 'Misumi', logo: '/assets/partners/2.png', link: 'https://kr.misumi-ec.com/' },
-    { name: 'Meviy', logo: '/assets/partners/3.png', link: 'https://meviy.misumi-ec.com/ko-kr/' },
+    { name: 'SeoulTech RISE', logo: '/assets/partners/2.png', link: 'https://rise.seoultech.ac.kr/' },
+    { name: 'Misumi', logo: '/assets/partners/3.png', link: 'https://kr.misumi-ec.com/' },
+    { name: 'Meviy', logo: '/assets/partners/4.png', link: 'https://meviy.misumi-ec.com/ko-kr/' },
     { name: 'Next Partner', logo: null, link: '#contact' }, // Placeholder
 ];
