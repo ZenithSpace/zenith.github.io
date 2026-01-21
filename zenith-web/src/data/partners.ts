cd5e79599c8c4d760e39d743ec659aa2192ac958
Ellian <dbsrn0125@naver.com>
2026-01-21 14:15:13 +0900

    feat: add Ministry of Education as a partner

--- zenith-web/src/data/partners.ts
+++ zenith-web/src/data/partners.ts
@@ -9,6 +9,7 @@ export const partnersData: Partner[] = [
     { name: 'SeoulTech', logo: '/assets/partners/1.webp', link: 'https://www.seoultech.ac.kr/' },
     { name: 'SeoulTech RISE', logo: '/assets/partners/2.webp', link: 'https://rise.seoultech.ac.kr/' },
     { name: 'Seoul Metropolitan Government', logo: '/assets/partners/seoul_metro.png', link: 'https://www.seoul.go.kr/', className: '-translate-y-1.5' },
+    { name: 'Ministry of Education', logo: '/assets/partners/moe_logo.webp', link: 'https://www.moe.go.kr/' },
     { name: 'The Seoul Institute', logo: '/assets/partners/seoul_institute.png', link: 'https://www.si.re.kr/' },
     { name: 'Denver Korea', logo: '/assets/partners/denver.jpg', link: 'http://www.denverkorea.co.kr/' },
     { name: 'Misumi', logo: '/assets/partners/3.webp', link: 'https://kr.misumi-ec.com/' },
